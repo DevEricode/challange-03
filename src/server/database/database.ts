716b20e3a5011ac48cdaadeb4e8024095d2b65fc
Eric Sampaio <ericstudy97@gmail.com>
2023-12-29 14:36:45 -0300

    feat: Created the database connection.

---
+++ src/server/database/database.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+class DatabaseConnection {
+    private uri: string;
+    constructor(uri: string) {
+        this.uri = uri;
+    }
+    public async connect() {
+        try {
+            await mongoose.connect(this.uri);
+            console.log('Connected to the bank!');
+        } catch (exception) {
+            console.error('Error connecting to the database', exception);
+        }
+    }
+    public async disconnect() {
+        try {
+            await mongoose.disconnect();
+        } catch (exception) {
+            console.error('Error disconnecting to the database', exception);
+        }
+    }
+}
+
+export default DatabaseConnection;
