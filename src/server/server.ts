d1a965e08765ba05a04a48e295f3d9f8fc2a1cba
Eric Sampaio <ericstudy97@gmail.com>
2024-01-02 11:29:52 -0300

    refactor: Removing the middleware from this file.

--- src/server/server.ts
+++ src/server/server.ts
@@ -7,15 +7,12 @@ import { router } from './routes/router';
 
 import DatabaseConnection from './database/database';
 
-import { validationSchemaMiddleware } from './shared/middleware/validationMiddleware';
 
 const db = new DatabaseConnection(process.env.URI!);
 
 db.connect();
 
 const server =  express();
-
-server.use(validationSchemaMiddleware);
 
 server.use(express.json());
 
