3d3c115cdec94d69a63859c75b18f1ed859ef879
Eric Sampaio <ericstudy97@gmail.com>
2024-01-02 11:30:24 -0300

    refactor: Adding validation middleware.

--- src/server/routes/router.ts
+++ src/server/routes/router.ts
@@ -2,6 +2,9 @@ import { Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { usersController } from '../controllers/users/UsersController';
 import { eventsController } from '../controllers/events/EventsController';
+import { validateSchemas } from '../shared/middleware/validationMiddleware';
+import { createUser } from '../shared/validation/users/userValidation';
+import { createEvent } from '../shared/validation/events/eventValidation';
 
 const router = Router();
 
@@ -9,10 +12,10 @@ router.get('/', (req, res) => {
     return res.status(StatusCodes.ACCEPTED).send('Teste!');
 });
 
-router.post('/users/sign-in', usersController.signIn);
-router.post('/users/sign-up', usersController.signUp);
+router.post('/users/sign-in',usersController.signIn);
+router.post('/users/sign-up',validateSchemas(createUser), usersController.signUp);
 
-router.post('/events', eventsController.createEvent);
+router.post('/events', validateSchemas(createEvent),eventsController.createEvent);
 router.get('/events', eventsController.getAllEvents);
 router.get('/events/:id', eventsController.getEventById);
 router.delete('/events/:day', eventsController.deleteEventByDay);
