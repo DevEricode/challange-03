402568f1f2a64988e0a6128ce41e479e78e7f20e
Eric Sampaio <ericstudy97@gmail.com>
2024-01-02 11:27:35 -0300

    feat: Created login functionality.

--- src/server/controllers/users/signIn.ts
+++ src/server/controllers/users/signIn.ts
@@ -1,9 +1,36 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { model } from 'mongoose';
+import bcrypt from 'bcrypt';
+import userSchema from '../../shared/models/userSchema';
+
+const collectionName = process.env.DB_COLLECTION1 || 'collection_users';
+
+const User = model('User', userSchema, collectionName);
 
 class SignInController {
-    signIn(req: Request, res: Response): void {
-        res.status(StatusCodes.OK).json({ message: 'Login bem-sucedido' });
+    async signIn(req: Request, res: Response): Promise<Response> {
+        try {
+            const { email, password } = req.body;
+
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Invalid credentials' });
+            }
+
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+
+            if (!isPasswordValid) {
+                return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Invalid credentials' });
+            }
+
+
+            return res.status(StatusCodes.OK).json({ message: 'Login successful' });
+        } catch (error) {
+            console.error('Error during login:', error);
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'An error occurred during login' });
+        }
     }
 }
 
