481e406e19d5bf1d36b5b9c9e47b9eb513ffa66c
Eric Sampaio <ericstudy97@gmail.com>
2024-01-02 11:27:03 -0300

    fix: Fixing the login bug.

--- src/server/controllers/users/signUp.ts
+++ src/server/controllers/users/signUp.ts
@@ -1,20 +1,23 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import userSchema from '../../shared/models/userSchema';
-import { MongoClient, ObjectId } from 'mongodb';
+import { ObjectId } from 'mongodb';
 import bcrypt from 'bcrypt';
 import { model } from 'mongoose';
 import 'dotenv/config';
 
-const User = model('User', userSchema);
+const collectionName = process.env.DB_COLLECTION1 || 'collection_users';
+
+const User = model('User', userSchema, collectionName);
 
 class SignUpController {
     async signUp(req: Request, res: Response): Promise<Response> {
         const userData = req.body;
+        userData.email = userData.email.toLowerCase();
 
         const newUser = new User({
             ...userData,
-            _id: new ObjectId(), 
+            _id: new ObjectId(),
         });
 
         try {
@@ -23,12 +26,8 @@ class SignUpController {
             return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Invalid Input.', details: error });
         }
 
-        const client = new MongoClient(process.env.URI!);
-
         try {
-            await client.connect();
-
-            const emailExists = await this.emailExistsInDatabase(client, userData.email);
+            const emailExists = await User.findOne({ email: userData.email });
             if (emailExists) {
                 return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Email already in use.' });
             }
@@ -36,21 +35,15 @@ class SignUpController {
             const hashedPassword = await bcrypt.hash(userData.password, 10);
             newUser.password = hashedPassword;
 
+            const hashedConfirmPassword = await bcrypt.hash(userData.confirmPassword, 10);
+            newUser.confirmPassword = hashedConfirmPassword;
+
             await newUser.save();
 
             return res.status(StatusCodes.CREATED).json({ message: 'User created.' });
-        } finally {
-
-            await client.close();
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'An error occurred.' });
         }
-    }
-
-    async emailExistsInDatabase(client: MongoClient, email: string): Promise<boolean> {
-        const db = client.db(process.env.DB_NAME);
-        const collection = db.collection(process.env.DB_COLLECTION!);
-
-        const existingUser = await collection.findOne({ email });
-        return !!existingUser;
     }
 }
 
