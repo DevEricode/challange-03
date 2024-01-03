a0796c8589746f499d6ff3d496b59257c66d4f39
Eric Sampaio <ericstudy97@gmail.com>
2024-01-03 13:25:26 -0300

    feat: Created the functionality to create events.

--- src/server/controllers/events/createEvent.ts
+++ src/server/controllers/events/createEvent.ts
@@ -1,11 +1,36 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import eventSchema from '../../shared/models/eventSchema';
+import { ObjectId } from 'mongodb';
+import { model } from 'mongoose';
+import 'dotenv/config';
 
-class CreateEvent {
+const collectionName = process.env.DB_COLLECTION2 || 'collection_events';
 
-    createEvent(req: Request, res: Response): void {
-        res.status(StatusCodes.OK).json({ message: 'Event created successfully.' });
+const Event = model('Event', eventSchema, collectionName);
+
+class createEventController {
+    async createEvent(req: Request, res: Response): Promise<Response> {
+        const eventData = req.body;
+
+        const newEvent = new Event({
+            ...eventData,
+            _id: new ObjectId(),
+        });
+
+        try {
+            await newEvent.validate();
+        } catch (error) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Invalid Input.', details: error });
+        }
+
+        try {
+            await newEvent.save();
+            return res.status(StatusCodes.OK).json({ message: 'Successful operation.' });
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'An error occurred.' });
+        }
     }
 }
 
-export const createEventInstance = new CreateEvent();
+export const createEventsInstance = new createEventController();
