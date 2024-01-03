b3c50442987816ed86e30a1c3d2e568d7a757a88
Eric Sampaio <ericstudy97@gmail.com>
2024-01-03 13:27:41 -0300

    refactor: Adding the id of whoever created the event in validation.

--- src/server/shared/validation/events/eventValidation.ts
+++ src/server/shared/validation/events/eventValidation.ts
@@ -5,4 +5,5 @@ import { description, dayOfWeek } from './eventRegex';
 export const createEvent = Joi.object({
     description: Joi.string().regex(description).min(6).required(),
     dayOfWeek: Joi.string().regex(dayOfWeek).min(3).required(),
+    userId: Joi.string().required()
 });
