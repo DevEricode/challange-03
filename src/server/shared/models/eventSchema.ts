e05d13eeab8adde6e4eeebc77157d17566e359b2
Eric Sampaio <ericstudy97@gmail.com>
2024-01-03 13:24:12 -0300

    refactor: Including the functionality to add the ID of whoever created the event.

--- src/server/shared/models/eventSchema.ts
+++ src/server/shared/models/eventSchema.ts
@@ -4,6 +4,7 @@ import Events from '../../shared/interface/eventsInterface';
 const eventSchema = new Schema<Events>({
     description: { type: String, required: true },
     dayOfWeek: { type: String, required: true },
+    userId: {type: String, required: true}
 });
 
 export default eventSchema;
