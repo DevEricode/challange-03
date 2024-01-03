dc4bdbef7a90cfa8f96aeec81067202b3e64c437
Eric Sampaio <ericstudy97@gmail.com>
2024-01-03 13:26:04 -0300

    feat: Created the functionality to retrieve events by day of the week.

---
+++ src/server/controllers/events/getEventsByDay.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+// import jwt, { JwtPayload } from 'jsonwebtoken';
+import eventSchema from '../../shared/models/eventSchema';
+import { model } from 'mongoose';
+
+const collectionName = process.env.DB_COLLECTION2 || 'collection_events';
+
+const Event = model('Event', eventSchema, collectionName);
+
+class getEventsByDayController {
+    async getEventsByDay(req: Request, res: Response): Promise<Response> {
+        const dayOfWeek = req.params.dayOfWeek;
+
+        const token = req.headers.authorization;
+        if (!token) {
+            return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Unauthorized.' });
+        }
+
+        let userId;
+
+        if (!dayOfWeek || typeof dayOfWeek !== 'string') {
+            return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Invalid data supplied.' });
+        }
+
+        try {
+            const events = await Event.find({ dayOfWeek, userId });
+            if (events.length === 0) {
+                return res.status(StatusCodes.NOT_FOUND).json({ message: 'Event not found.' });
+            }
+
+            const responseBody = events.map(event => ({
+                id: event._id,
+                description: event.description,
+                dayOfWeek: event.dayOfWeek,
+                userId: event.userId,
+            }));
+
+            return res.status(StatusCodes.OK).json({ message: 'Successful operation.', events: responseBody });
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'An error occurred.' });
+        }
+    }
+}
+
+export const getEventsByDayInstance = new getEventsByDayController();
