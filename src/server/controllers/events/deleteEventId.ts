4cba2ccee946e15da3dd2d292005c88e48304e50
Eric Sampaio <ericstudy97@gmail.com>
2024-01-03 13:45:47 -0300

    feat: Created the controller to delete the event by id.

--- src/server/controllers/events/deleteEventId.ts
+++ src/server/controllers/events/deleteEventId.ts
@@ -1,11 +1,31 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import eventSchema from '../../shared/models/eventSchema';
+import { model } from 'mongoose';
 
-class DeleteEventId {
+const collectionName = process.env.DB_COLLECTION2 || 'collection_events';
 
-    deleteEventId(req: Request, res: Response): void {
-        res.status(StatusCodes.OK).json({ message: 'Event deleted successfully.' });
+const Event = model('Event', eventSchema, collectionName);
+
+class deleteEventByIdController {
+    async deleteEventById(req: Request, res: Response): Promise<Response> {
+        const eventId = req.params.id;
+
+        if (!eventId || typeof eventId !== 'string') {
+            return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Invalid ID supplied.' });
+        }
+
+        try {
+            const event = await Event.findByIdAndDelete(eventId);
+            if (!event) {
+                return res.status(StatusCodes.NOT_FOUND).json({ message: 'Event not found.' });
+            }
+
+            return res.status(StatusCodes.NO_CONTENT).send();
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'An error occurred.' });
+        }
     }
 }
 
-export const deleteEventInstance = new DeleteEventId();
+export const deleteEventByIdInstance = new deleteEventByIdController();
