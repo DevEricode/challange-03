a15e0dbff6e3ebe3850874101084a747a0d2d050
Eric Sampaio <ericstudy97@gmail.com>
2024-01-03 13:45:14 -0300

    feat: Created the controller to pull events by id.

--- src/server/controllers/events/getEventById.ts
+++ src/server/controllers/events/getEventById.ts
@@ -1,11 +1,38 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import eventSchema from '../../shared/models/eventSchema';
+import { model } from 'mongoose';
 
-class GetEventById {
+const collectionName = process.env.DB_COLLECTION2 || 'collection_events';
 
-    getEventById(req: Request, res: Response): void {
-        res.status(StatusCodes.OK).json({ message: 'Getting event by id.' });
+const Event = model('Event', eventSchema, collectionName);
+
+class getEventByIdController {
+    async getEventById(req: Request, res: Response): Promise<Response> {
+        const eventId = req.params.id;
+
+        if (!eventId || typeof eventId !== 'string') {
+            return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Invalid data supplied.' });
+        }
+
+        try {
+            const event = await Event.findById(eventId);
+            if (!event) {
+                return res.status(StatusCodes.NOT_FOUND).json({ message: 'Event not found.' });
+            }
+
+            const responseBody = {
+                id: event._id,
+                description: event.description,
+                dayOfWeek: event.dayOfWeek,
+                userId: event.userId,
+            };
+
+            return res.status(StatusCodes.OK).json({ message: 'Successful operation.', event: responseBody });
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'An error occurred.' });
+        }
     }
 }
 
-export const getEventByIdInstance = new GetEventById();
+export const getEventByIdInstance = new getEventByIdController();
