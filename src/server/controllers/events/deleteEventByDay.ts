c5a3449bc314598e856a1bc046f50221381e412b
Eric Sampaio <ericstudy97@gmail.com>
2024-01-03 13:44:44 -0300

    feat: Created a controller to delete an event by day of the week.

--- src/server/controllers/events/deleteEventByDay.ts
+++ src/server/controllers/events/deleteEventByDay.ts
@@ -1,11 +1,31 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import eventSchema from '../../shared/models/eventSchema';
+import { model } from 'mongoose';
 
-class DeleteEventByDay {
+const collectionName = process.env.DB_COLLECTION2 || 'collection_events';
 
-    deleteEventByDay(req: Request, res: Response): void {
-        res.status(StatusCodes.OK).json({ message: 'Events on the day successfully deleted.' });
+const Event = model('Event', eventSchema, collectionName);
+
+class deleteEventsByDayController {
+    async deleteEventsByDay(req: Request, res: Response): Promise<Response> {
+        const dayOfWeek = req.query.dayOfWeek;
+
+        if (!dayOfWeek || typeof dayOfWeek !== 'string') {
+            return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Invalid data supplied.' });
+        }
+
+        try {
+            const events = await Event.deleteMany({ dayOfWeek });
+            if (events.deletedCount === 0) {
+                return res.status(StatusCodes.NOT_FOUND).json({ message: 'Event not found.' });
+            }
+
+            return res.status(StatusCodes.OK).json({ message: 'List of deleteDailyEvents', deletedEvents: events.deletedCount });
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'An error occurred.' });
+        }
     }
 }
 
-export const deleteEventByDayInstance = new DeleteEventByDay();
+export const deleteEventsByDayInstance = new deleteEventsByDayController();
