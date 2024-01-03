32ba24338c84ad99a91a9a0bd064730d1d851067
Eric Sampaio <ericstudy97@gmail.com>
2024-01-03 13:29:27 -0300

    refactor: Adding the validation token when logging in.

--- src/server/controllers/users/signIn.ts
+++ src/server/controllers/users/signIn.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { model } from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import userSchema from '../../shared/models/userSchema';
 
 const collectionName = process.env.DB_COLLECTION1 || 'collection_users';
@@ -13,10 +14,18 @@ class SignInController {
         try {
             const { email, password } = req.body;
 
+            if (!password) {
+                return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Password is required' });
+            }
+
             const user = await User.findOne({ email });
 
             if (!user) {
                 return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Invalid credentials' });
+            }
+
+            if (!user.password) {
+                return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'An error occurred during login' });
             }
 
             const isPasswordValid = await bcrypt.compare(password, user.password);
@@ -25,10 +34,14 @@ class SignInController {
                 return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Invalid credentials' });
             }
 
+            const token = jwt.sign({ userId: user._id }, process.env.SECRET as string, {
+                expiresIn: '4w',
+            });
+
+            res.header('Authorization', `Bearer ${token}`);
 
             return res.status(StatusCodes.OK).json({ message: 'Login successful' });
         } catch (error) {
-            console.error('Error during login:', error);
             return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'An error occurred during login' });
         }
     }
