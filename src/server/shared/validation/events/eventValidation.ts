f7fe49291193a02f1ea01822ca06c2d161b05432
Eric Sampaio <ericstudy97@gmail.com>
2024-01-04 11:04:57 -0300

    refactor: Removing userId from validation.

--- src/server/shared/validation/events/eventValidation.ts
+++ src/server/shared/validation/events/eventValidation.ts
@@ -5,5 +5,4 @@ import { description, dayOfWeek } from './eventRegex';
 export const createEvent = Joi.object({
     description: Joi.string().regex(description).min(6).required(),
     dayOfWeek: Joi.string().regex(dayOfWeek).min(3).required(),
-    userId: Joi.string().required()
 });
