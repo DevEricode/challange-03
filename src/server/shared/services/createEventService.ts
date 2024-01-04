72a39d88d9c928a931683a9f4564ab8fe90a3686
Eric Sampaio <ericstudy97@gmail.com>
2024-01-04 14:51:13 -0300

    feat: The service for creating events was created.

---
+++ src/server/shared/services/createEventService.ts
@@ -0,0 +1,21 @@
+import eventSchema from '../models/eventSchema';
+import { ObjectId } from 'mongodb';
+import { model } from 'mongoose';
+import 'dotenv/config';
+
+
+const collectionName = process.env.DB_COLLECTION2 || 'collection_events';
+const Event = model('Event', eventSchema, collectionName);
+
+export class EventService {
+    async createEvent(eventData: Event, userId: string) {
+        const newEvent = new Event({
+            ...eventData,
+            _id: new ObjectId(),
+            userId: userId,
+        });
+
+        await newEvent.save();
+        return newEvent;
+    }
+}
