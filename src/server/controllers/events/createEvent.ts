21d5904ed9a5f5aff7ca5fe6c12a2824e8fe2656
Eric Sampaio <ericstudy97@gmail.com>
2024-01-04 11:04:30 -0300

    fix: Fixing the bug of generating userId.

--- src/server/controllers/events/createEvent.ts
+++ src/server/controllers/events/createEvent.ts
@@ -5,30 +5,35 @@ import { ObjectId } from 'mongodb';
 import { model } from 'mongoose';
 import 'dotenv/config';
 
+interface RequestWithUser extends Request {
+    userId?: string;
+}
+
 const collectionName = process.env.DB_COLLECTION2 || 'collection_events';
 
 const Event = model('Event', eventSchema, collectionName);
 
 class createEventController {
-    async createEvent(req: Request, res: Response): Promise<Response> {
+    async createEvent(req: RequestWithUser, res: Response): Promise<Response> {
         const eventData = req.body;
 
-        const newEvent = new Event({
-            ...eventData,
-            _id: new ObjectId(),
-        });
-
-        try {
-            await newEvent.validate();
-        } catch (error) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Invalid Input.', details: error });
+        if (!req.userId) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ message: 'userId is required.' });
         }
 
         try {
+
+            const newEvent = new Event({
+                ...eventData,
+                _id: new ObjectId(),
+                userId: req.userId,
+            });
+
             await newEvent.save();
+
             return res.status(StatusCodes.OK).json({ message: 'Successful operation.' });
         } catch (error) {
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'An error occurred.' });
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'An error occurred.', details: error });
         }
     }
 }
