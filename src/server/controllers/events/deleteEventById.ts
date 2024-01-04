a1abafe467ef1eb7d28e39f424c243b6b0740fe8
Eric Sampaio <ericstudy97@gmail.com>
2024-01-04 14:50:24 -0300

    refactor: I separated the services from the controller.

---
+++ src/server/controllers/events/deleteEventById.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { EventService } from '../../shared/services/deleteEventByIdService';
+
+class DeleteEventByIdController {
+    eventService: EventService;
+
+    constructor(eventService: EventService) {
+        this.eventService = eventService;
+    }
+
+    async deleteEventById(req: Request, res: Response): Promise<Response> {
+        const eventId = req.params.id;
+
+        if (!eventId || typeof eventId !== 'string') {
+            return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Invalid ID supplied.' });
+        }
+
+        try {
+            const event = await this.eventService.deleteEventById(eventId);
+            if (!event) {
+                return res.status(StatusCodes.NOT_FOUND).json({ message: 'Event not found.' });
+            }
+
+            return res.status(StatusCodes.NO_CONTENT).send();
+        } catch (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'An error occurred.' });
+        }
+    }
+}
+
+export const deleteEventByIdInstance = new DeleteEventByIdController(new EventService());
