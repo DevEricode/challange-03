c9089a507b1149387d2273cf8b8a03df68242bf8
Eric Sampaio <ericstudy97@gmail.com>
2024-01-05 15:08:34 -0300

    fix: The bug of searching with an id outside the format and when there is no id.

--- src/server/controllers/events/deleteEventById.ts
+++ src/server/controllers/events/deleteEventById.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { EventService } from '../../shared/services/events/deleteEventByIdService';
+import throwErr from '../../shared/errors/handleError';
 
 class DeleteEventByIdController {
     eventService: EventService;
@@ -12,25 +13,16 @@ class DeleteEventByIdController {
     async deleteEventById(req: Request, res: Response): Promise<Response> {
         const eventId = req.params.id;
 
-        if (!eventId || typeof eventId !== 'string') {
-            return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Invalid ID supplied.' });
+        const event = await this.eventService.deleteEventById(eventId);
+
+        if (!event) {
+
+            throwErr(6);
+
         };
 
-        try {
-
-            const event = await this.eventService.deleteEventById(eventId);
-
-            if (!event) {
-
-                return res.status(StatusCodes.NOT_FOUND).json({ message: 'Event not found.' });
-
-            };
-
-            return res.status(StatusCodes.NO_CONTENT).send();
+        return res.status(StatusCodes.NO_CONTENT).send();
             
-        } catch (error) {
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'An error occurred.' });
-        };
     };
 };
 
--- src/server/shared/services/events/deleteEventByIdService.ts
+++ src/server/shared/services/events/deleteEventByIdService.ts
@@ -1,12 +1,18 @@
-import { model } from 'mongoose';
+import mongoose, { model } from 'mongoose';
 import eventSchema from '../../models/eventSchema';
 import 'dotenv/config';
+import throwErr from '../../errors/handleError';
 
 const collectionName = process.env.DB_COLLECTION2 || 'collection_events';
 const Event = model('Event', eventSchema, collectionName);
 
 export class EventService {
     async deleteEventById(eventId: string) {
+
+        if (!mongoose.Types.ObjectId.isValid(eventId)) {
+            throwErr(4);
+        }
+        
         return await Event.findByIdAndDelete(eventId);
     };
 };
